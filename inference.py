a1dacb40232e9057a388402ec16467fc26b25e67
pz <zhaopuglory@gmail.com>
2024-05-29 15:35:18 -0400

    inference update

--- inference.py
+++ inference.py
@@ -1,19 +1,12 @@
-from diffusers import AutoPipelineForText2Image,DiffusionPipeline
+from diffusers import DiffusionPipeline
 import torch
-	
 import time
 
-
-#pipeline = AutoPipelineForText2Image.from_pretrained(
-#	"runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16, variant="fp16").to("cuda")
-
-
 #pipeline = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", use_safetensors=True)
-pipeline = DiffusionPipeline.from_pretrained("./ckpts", use_safetensors=True).to("cuda")
-
+pipeline = DiffusionPipeline.from_pretrained("piuzha/efficient_sd/", use_safetensors=True).to("cuda")
 
 image = pipeline(
-	"A cute cat under sofa, detailed, 8k", num_inference_steps=25).images[0]
+	"An astronaut riding a horse, detailed, 8k", num_inference_steps=25).images[0]
 
 image.save('test.png')
 
